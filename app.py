e72dc6f4ea790a67359ba5204fcf521cdcd357d0
Andrew Miller <andrew.miller@founders4schools.org.uk>
2017-03-12 16:31:43 +0000

    add generic structured message

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def receivedPostback(event):
 
     # The 'payload' param is a developer-defined field which is set in a postback 
     # button for Structured Messages.
-    payload = event.postback.payload
+    payload = event.get('postback', {}).get('payload')
 
     app.logger.info("Received postback for user %d and page %d with payload '%s' at %d", 
                     senderID, recipientID, payload, timeOfPostback)
